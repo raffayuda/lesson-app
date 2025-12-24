944c782a4e59adac526ba1ff29fe9837afffd2d4
raffayuda <rafffa0206@gmail.com>
2025-12-24 15:05:06 +0700

    feat: localize table header from 'Murid' to 'Siswa' in Dashboard and Schedules_backup components

--- frontend/src/pages/admin/Schedules_backup.svelte
+++ frontend/src/pages/admin/Schedules_backup.svelte
@@ -702,7 +702,7 @@
                                 <tr>
                                     <th
                                         class="px-4 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider"
-                                        >Murid</th
+                                        >Siswa</th
                                     >
                                     
                                     <th
